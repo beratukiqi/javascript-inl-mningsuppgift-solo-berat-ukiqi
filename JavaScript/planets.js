cea6ce8251c0dd67f3d5554e2da3caf0a97d0128
beratukiqi <berat.u@live.se>
2023-02-24 09:48:44 +0100

    Fixes

--- JavaScript/planets.js
+++ JavaScript/planets.js
@@ -5,6 +5,8 @@ let nextButton = document.querySelector('.pagination__next');
 let prevButton = document.querySelector('.pagination__back');
 let planetTextList = document.querySelectorAll('.planets p');
 
+
+
 async function renderPlanetData(id) {
     // Hides all planet data from UI before rendering new data
     let planetBlocksBefore = document.querySelectorAll('.fadeInDown');
@@ -21,6 +23,7 @@ async function renderPlanetData(id) {
     let planetMoons = document.querySelector('.moons');
     let planet = await fetchPlanet(id);
 
+
     let name = planet.name;
     let latinName = planet.latinName;
     let desc = planet.desc;
@@ -30,6 +33,8 @@ async function renderPlanetData(id) {
     let dist = planet.distance;
     let day = planet.temp.day;
     let night = planet.temp.night;
+
+    handlePlanetColor(id);
 
     // UI for planet header
     planetHeader.innerHTML = `
@@ -84,59 +89,75 @@ async function renderPlanetData(id) {
 }
 
 function handlePlanetColor(id) {
-    let planet = document.querySelector(`.planet`);
-
-    switch (id) {
+    let planet = document.querySelector('.planet');
+    console.log(parseInt(id));
+    switch (parseInt(id)) {
         case 0:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #F9C80E40';
             planet.style.backgroundColor = '#F9C80E';
+            console.log('case 0');
             break;
 
         case 1:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #88888840';
             planet.style.backgroundColor = '#888888';
+            console.log('case 1');
             break;
 
         case 2:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #E7CDCD40';
             planet.style.backgroundColor = '#E7CDCD';
+            console.log('case 2');
             break;
 
         case 3:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #428ED440';
             planet.style.backgroundColor = '#428ED4';
+            console.log('case 3')
             break;
 
         case 4:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #EF5F5F40';
             planet.style.backgroundColor = '#EF5F5F';
+            console.log('case 4')
             break;
 
         case 5:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #E2946840';
             planet.style.backgroundColor = '#E29468';
+            console.log('case 5')
             break;
 
         case 6:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #C7AA7240';
             planet.style.backgroundColor = '#C7AA72';
+            console.log('case 6')
             break;
 
         case 7:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #C9D4F140';
             planet.style.backgroundColor = '#C9D4F1';
+            console.log('case 7')
             break;
         case 8:
             planet.style.boxShadow = '5rem 0rem 50rem 0px #7A91A740';
             planet.style.backgroundColor = '#7A91A7';
+            console.log('case 8')
             break;
 
     }
 }
 
-currentPlanet = localStorage.getItem('planetID');
+currentPlanet = parseInt(localStorage.getItem('planetID'));
+console.log(currentPlanet, 'current planet');
 renderPlanetData(currentPlanet);
-handlePlanetColor(currentPlanet)
+
+// Removes buttons if user starts at end of list
+if (currentPlanet === 1) {
+    prevButton.style.display = 'none';
+} else if (currentPlanet === 8) {
+    nextButton.style.display = 'none';
+}
 
 planetTextList.forEach((planet, id) => {
     setTimeout(() => {
@@ -148,16 +169,13 @@ planetTextList.forEach((planet, id) => {
 prevButton.addEventListener('click', () => {
     window.scrollTo(0, 0);
     // Handles the button displays
-    console.log(currentPlanet, 'pre click');
-    if (currentPlanet === 1) {
-        prevButton.style.display = 'none';
-    } else if (currentPlanet === 8) {
+    if (currentPlanet === 8) {
         nextButton.style.display = 'revert';
+    } else if (currentPlanet === 2) {
+        prevButton.style.display = 'none';
     }
 
     currentPlanet--;
-    console.log(currentPlanet, 'post click');
-
     handlePlanetColor(currentPlanet);
     renderPlanetData(currentPlanet);
 });
@@ -165,16 +183,16 @@ prevButton.addEventListener('click', () => {
 nextButton.addEventListener('click', () => {
         window.scrollTo(0, 0);
         // Handles the button displays
-        console.log(currentPlanet, 'pre click');
-        
-        if (currentPlanet === 7) {
-            nextButton.style.display = 'none';
-        } else if (currentPlanet === 0) {
+        if (currentPlanet === 1) {
             prevButton.style.display = 'revert';
+        } else if (currentPlanet === 7) {
+            nextButton.style.display = 'none';
         }
+        
         currentPlanet++;
-        console.log(currentPlanet, 'post click');
-
         handlePlanetColor(currentPlanet);
         renderPlanetData(currentPlanet);
     });
+
+// handlePlanetColor(currentPlanet)
+
